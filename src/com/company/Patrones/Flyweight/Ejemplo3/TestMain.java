1e0009f63a3ba25d49418d142d5d09815026c60d
Erick Adriel Vaernet <erickadrielnet@gmail.com>
2022-03-08 13:38:14 -0300

    Agregado 3er ejemplo a Flyweight

--- src/com/company/Patrones/Flyweight/Ejemplo3/TestMain.java
+++ src/com/company/Patrones/Flyweight/Ejemplo3/TestMain.java
@@ -1,5 +1,8 @@
 package com.company.Patrones.Flyweight.Ejemplo3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestMain {
 
 
@@ -8,19 +11,25 @@ public class TestMain {
 
          Arbol a1= new Arbol("Ornamentanl",200,400,"verde");
          Arbol a2= new Arbol("Frutal",500,300,"rojo");
-         Arbol a3;
-         Arbol a4;
+        List<Arbol> listaArboles =new ArrayList<>();
 
         for (int i=0; i<500000;i++){
-            a3=ArbolFactory.getArbol(a1.getTipo(),a1.getAlto(),a1.getAncho(),a1.getColor());
-            a4=ArbolFactory.getArbol(a2.getTipo(),a2.getAlto(),a2.getAncho(),a2.getColor());
-            System.out.println(a3.toString());
-            System.out.println(a4.toString());
+            //Con Flyweight
+
+            listaArboles.add(ArbolFactory.getArbol(a1.getTipo(),a1.getAlto(),a1.getAncho(),a1.getColor()));
+            listaArboles.add(ArbolFactory.getArbol(a2.getTipo(),a2.getAlto(),a2.getAncho(),a2.getColor()));
+
+
+            //Sin Flyweight
+            /*
+            listaArboles.add(new Arbol("Ornamentanl",200,400,"verde"));
+            listaArboles.add(new Arbol("Frutal",500,300,"rojo"));
+             */
         }
+        System.out.println(listaArboles.size());
 
         Runtime runtime = Runtime.getRuntime();
         System.out.println("Memoria usada: " + (runtime.totalMemory() - runtime.freeMemory()) / (1024 * 1024));
-
 
 
     }
