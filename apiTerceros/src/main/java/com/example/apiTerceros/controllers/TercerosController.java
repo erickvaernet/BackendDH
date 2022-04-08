c22c9b0248923a4a26c569771cf5ccc8e376959a
Erick Adriel Vaernet <erickadrielnet@gmail.com>
2022-04-08 12:22:50 -0300

    apitercero

--- apiTerceros/src/main/java/com/example/apiTerceros/controllers/TercerosController.java
+++ apiTerceros/src/main/java/com/example/apiTerceros/controllers/TercerosController.java
@@ -1,5 +1,8 @@
 package com.example.apiTerceros.controllers;
 
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.apache.http.HttpResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -11,7 +14,15 @@ public class TercerosController {
     @GetMapping("/{name}")
     public ResponseEntity<String> getCountry(@PathVariable String name){
         RestTemplate restTemplate= new RestTemplate();
-        String country= restTemplate.getForObject("https://restcountries.com/v3.1/name/{name}",String.class);
+        String country= restTemplate.getForObject("https://restcountries.com/v3.1/name/"+name,String.class);
         return ResponseEntity.ok(country);
+    }
+
+    @GetMapping("/unirest/{name}")
+    public ResponseEntity<String> getCountry2(@PathVariable String name) throws UnirestException {
+        String response=null;
+
+        response=Unirest.get("https://restcountries.com/v3.1/name/"+name).asString().toString();
+        return ResponseEntity.ok();
     }
 }
