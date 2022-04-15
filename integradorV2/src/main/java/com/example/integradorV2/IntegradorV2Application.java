45ad4267bc37f8cdf4ac34ad724890e8793f0914
Erick Adriel Vaernet <erickadrielnet@gmail.com>
2022-04-15 13:39:20 -0300

    Creado proyecto Integradorv2

---
+++ integradorV2/src/main/java/com/example/integradorV2/IntegradorV2Application.java
@@ -0,0 +1,13 @@
+package com.example.integradorV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegradorV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegradorV2Application.class, args);
+	}
+
+}
