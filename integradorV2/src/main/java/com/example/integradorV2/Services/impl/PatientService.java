0202e0a11c46e390d2762a730587ddb0cbb17da1
Erick Adriel Vaernet <erickadrielnet@gmail.com>
2022-04-17 15:59:09 -0300

    Integrador V2= metodo deleteById() agregado a PatientServices

--- integradorV2/src/main/java/com/example/integradorV2/Services/impl/PatientService.java
+++ integradorV2/src/main/java/com/example/integradorV2/Services/impl/PatientService.java
@@ -34,7 +34,7 @@ public class PatientService implements IPatientService {
 
     @Override
     public void deleteById(Long id) {
-
+        patientRepository.deleteById(id);
     }
 
     @Override
