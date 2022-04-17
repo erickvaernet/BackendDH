a9a78a98ee242993e5f3907d9af3260fe150f91a
Erick Adriel Vaernet <erickadrielnet@gmail.com>
2022-04-17 15:48:09 -0300

    Integrador V2= PatientDTO completo

--- integradorV2/src/main/java/com/example/integradorV2/DTO/PatientDTO.java
+++ integradorV2/src/main/java/com/example/integradorV2/DTO/PatientDTO.java
@@ -1,4 +1,27 @@
 package com.example.integradorV2.DTO;
 
+import com.example.integradorV2.Entities.Address;
+import com.example.integradorV2.Entities.Appointment;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class PatientDTO {
+    private Long id;
+    private Integer DNI;
+    private String name;
+    private String lastName;
+    private String email;
+    private LocalDate entryDate;
+    private Address address;
+    private Set<Appointment> appointments;
 }
