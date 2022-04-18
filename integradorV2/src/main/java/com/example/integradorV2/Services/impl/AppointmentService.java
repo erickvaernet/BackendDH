cbc00de29cff07f40586bd6733dce72f90a9112c
Erick Adriel Vaernet <erickadrielnet@gmail.com>
2022-04-17 23:07:08 -0300

    Integrador V2= AppointmentService creado

--- integradorV2/src/main/java/com/example/integradorV2/Services/impl/AppointmentService.java
+++ integradorV2/src/main/java/com/example/integradorV2/Services/impl/AppointmentService.java
@@ -1,12 +1,18 @@
 package com.example.integradorV2.Services.impl;
 
 import com.example.integradorV2.DTO.AppointmentDTO;
+import com.example.integradorV2.DTO.DentistDTO;
+import com.example.integradorV2.Entities.Appointment;
+import com.example.integradorV2.Entities.Dentist;
 import com.example.integradorV2.Persistence.IAppointmentRepository;
 import com.example.integradorV2.Services.IAppointmentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AppointmentService implements IAppointmentService {
@@ -15,27 +21,52 @@ public class AppointmentService implements IAppointmentService {
 
 
     @Override
-    public AppointmentDTO save(AppointmentDTO obj) {
-        return null;
+    public AppointmentDTO save(AppointmentDTO appointmentDTO) {
+        Appointment newAppointment= mapToEntity(appointmentDTO);
+        newAppointment=appointmentRepository.save(newAppointment);
+        return mapToDTO(newAppointment);
     }
 
     @Override
-    public AppointmentDTO update(Long id, AppointmentDTO obj) {
-        return null;
+    public AppointmentDTO update(Long id, AppointmentDTO appointmentDTO) {
+        AppointmentDTO appointmentToUpdate=findById(id);
+        //Comprobaciones
+        if(appointmentDTO==null || appointmentToUpdate==null) return null;
+        //Asignaciones
+        if(appointmentDTO.getPatient()!=null) appointmentToUpdate.setPatient(appointmentDTO.getPatient());
+        if(appointmentDTO.getDentist()!=null) appointmentToUpdate.setDentist(appointmentDTO.getDentist());
+        if(appointmentDTO.getDateTime()!=null) appointmentToUpdate.setDateTime(appointmentDTO.getDateTime());
+        //Guardado
+        appointmentRepository.save(mapToEntity(appointmentToUpdate));
+        return appointmentToUpdate;
     }
 
     @Override
     public AppointmentDTO findById(Long id) {
-        return null;
+        Optional<Appointment> optionalAppointment= appointmentRepository.findById(id);
+        return optionalAppointment.map(this::mapToDTO).orElse(null);
     }
 
     @Override
     public void deleteById(Long id) {
-
+        appointmentRepository.deleteById(id);
     }
 
     @Override
     public List<AppointmentDTO> findAll() {
-        return null;
+        List<AppointmentDTO> appointmentDTOList=new ArrayList<>();
+        appointmentRepository.findAll()
+                .forEach(
+                        (appointment)->appointmentDTOList.add(mapToDTO( appointment ))
+                );
+        return appointmentDTOList;
+    }
+
+    //Mappers
+    private AppointmentDTO mapToDTO(Appointment appointment){
+        return new ObjectMapper().convertValue(appointment, AppointmentDTO.class);
+    }
+    private Appointment mapToEntity(AppointmentDTO appointmentDto){
+        return new ObjectMapper().convertValue(appointmentDto, Appointment.class);
     }
 }
