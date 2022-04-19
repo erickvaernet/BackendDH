f2c9f91285b13c98820b91d68f907a70df7654d2
Erick Adriel Vaernet <erickadrielnet@gmail.com>
2022-04-19 15:19:29 -0300

    Integrador V2= Agregada excepciones

--- integradorV2/src/main/java/com/example/integradorV2/Exceptions/GlobalExceptionHandler.java
+++ integradorV2/src/main/java/com/example/integradorV2/Exceptions/GlobalExceptionHandler.java
@@ -1,19 +1,46 @@
 package com.example.integradorV2.Exceptions;
 
 import org.apache.log4j.Logger;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletRequest;
+
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     private static Logger logger= Logger.getLogger(GlobalExceptionHandler.class);
-    @ExceptionHandler
-    public ResponseEntity<?> allErrors(Exception ex, WebRequest req){
-        logger.error(ex.getMessage());
-        return  ResponseEntity.internalServerError()
-                .body("Error: "+ex.getMessage());
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler //Manejador global en caso de que no recaiga en ninguna otra excepcion
+    @ResponseBody
+    public ErrorMessage allErrors(Exception ex, HttpServletRequest req){
+        return logErrorMessage(req.getRequestURI(),ex.getMessage());
     }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({EntityNotFoundException.class})
+    @ResponseBody
+    public ErrorMessage notFoundException(HttpServletRequest req,Exception ex){
+        return logErrorMessage(req.getRequestURI(),ex.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({InvalidIdException.class})
+    @ResponseBody
+    public ErrorMessage invalidIDException(HttpServletRequest req,Exception ex){
+        return logErrorMessage(req.getRequestURI(),ex.getMessage());
+    }
+
+    private ErrorMessage logErrorMessage(String uri, String message){
+        ErrorMessage errorMessage =new ErrorMessage(uri,message);
+        logger.error(errorMessage.toString());
+        return errorMessage;
+    }
+
+
 }
