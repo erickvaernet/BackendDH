26dbbf623eb27705e078383157e6d836f087bb72
Erick Adriel Vaernet <erickadrielnet@gmail.com>
2022-04-23 18:53:17 -0300

    Integrador V2=eliminar importaciones innecesarias

--- integradorV2/src/main/java/com/example/integradorV2/IntegradorV2Application.java
+++ integradorV2/src/main/java/com/example/integradorV2/IntegradorV2Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 @SpringBootApplication
